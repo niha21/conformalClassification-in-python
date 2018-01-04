da3cefde90b02808ca0061453ae96658f699476e
Niharika <niharika.gauraha@farmbio.uu.se>
2018-01-04 20:51:14 +0100

    some updates in par TCP

--- conformalClassification/par_tcp.py
+++ conformalClassification/par_tcp.py
@@ -7,7 +7,7 @@ from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 import sys
 import multiprocessing
-import config
+import conformalClassification.config as config
 
 
 #############################################
